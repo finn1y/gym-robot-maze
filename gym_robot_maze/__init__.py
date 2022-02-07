d3f67bdca111f16c463c8bacdc583436373acfc0
Finn <finnmiddletonbaird@gmail.com>
2022-02-07 18:19:30 +0000

    Updated setup.py wit author and url

--- gym_robot_maze/__init__.py
+++ gym_robot_maze/__init__.py
@@ -3,6 +3,6 @@
 from gym.envs.registration import register
 
 register(
-    id='robot-maze-v0',
-    entry_point='gym_robot_maze.envs:MazeEnv',
+    id="robot-maze-v0",
+    entry_point="gym_robot_maze.envs:MazeEnv",
 )
--- setup.py
+++ setup.py
@@ -5,5 +5,7 @@ from setuptools import setup
 setup(
     name = 'gym_robot_maze',
     version = '0.0.1',
+    url="https://github.com/finn1y/gym-robot-maze",
+    author="Finn Middleton-Baird",
     install_requires = ['gym', 'pygame', 'numpy']
 )
